955c46007b62432b858d07da239ffa904a24af35
Don Smith <don.c.smith@gmail.com>
2018-08-18 20:53:16 -0500

    cpanfile version locker

--- app/scripts/gen-cpanfile.pl
+++ app/scripts/gen-cpanfile.pl
@@ -5,25 +5,50 @@ use warnings;
 use CPAN;
 use Data::Printer;
 
-sub requires {
+my @wanted_modules = ();
+my $header_out;
+my $last_base;
+
+sub checkRequire {
   my ($mod, $version) = @_;
   my $mo = CPAN::Shell->expandany($mod);
   print "Checking for $mod... ";
   if ($mo) {
+    (my $base = $mod) =~ s/^([^:]+).*$/$1/xsm;
+    if ($last_base && $base ne $last_base) {
+      print STDERR "\n";
+    }
+    $last_base = $base;
     my $our_version = $mo->inst_version;
     my $cpan_version = $mo->cpan_version;
     print "found $our_version (CPAN offers $cpan_version).\n";
-    print STDERR <<_CPAN;
-require "$mod", ">= $our_version"; # CPAN = $cpan_version;
-_CPAN
+    if (!$header_out) {
+      print STDERR "# This cpanfile was auto-generated\n";
+      $header_out = 1;
+    }
+    print STDERR "requires '$mod', '>= $our_version'; # CPAN offers $cpan_version;\n";
   } else {
     print "NOT INSTALLED!\n";
   }
 }
 
+sub requires {
+  my ($mod, $version) = @_;
+  push @wanted_modules, {
+    mod_name => $mod,
+    version => $version
+  };
+}
 
 if (-f 'cpanfile') {
   require './cpanfile';
 } else {
-  print "No cpanfile found.  Run this script from the director it is located in.\n"
+  print "No cpanfile found.  Run this script from the director it is located in.\n";
+  die;
 }
+
+map {
+  checkRequire($_->{mod_name}, $_->{version})
+} sort {
+  $a->{mod_name} cmp $b->{mod_name}
+} @wanted_modules;
